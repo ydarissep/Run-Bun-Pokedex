ca73925adc27c04dd69724fd65c46c03c2726976
ydarissep <60859033+ydarissep@users.noreply.github.com>
2023-03-19 23:43:48 +0100

    Add files via upload

--- src/utility.js
+++ src/utility.js
@@ -57,7 +57,7 @@ async function fetchTypeChart(){
 
 
 async function forceUpdate(){
-    const update = 1
+    const update = 2
     if(localStorage.getItem("update") != `${update} R&B`){
         await localStorage.clear()
         await localStorage.setItem("update", `${update} R&B`)
