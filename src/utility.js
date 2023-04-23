e2f8667a671e6396bbcddb272e4dbe388a20b137
ydarissep <60859033+ydarissep@users.noreply.github.com>
2023-04-24 01:42:15 +0200

    Update utility.js

--- src/utility.js
+++ src/utility.js
@@ -61,7 +61,7 @@ async function fetchTypeChart(){
 
 
 async function forceUpdate(){
-    const update = 7
+    const update = 8
     if(localStorage.getItem("update") != `${update} R&B`){
         await localStorage.clear()
         await localStorage.setItem("update", `${update} R&B`)
